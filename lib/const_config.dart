94c0a53b26d6de7c9063323286ef04ab68867a81
EfbSm5 <3325989547@qq.com>
2024-10-13 19:04:56 +0800

    成功跑起来了

---
+++ lib/const_config.dart
@@ -0,0 +1,9 @@
+import 'package:x_amap_base/x_amap_base.dart';
+
+class ConstConfig {
+  static const AMapApiKey amapApiKeys = AMapApiKey(
+      androidKey: '924d1353ea34a96761f2e040c224e445',
+      iosKey: '4dfdec97b7bf0b8c13e94777103015a9');
+  static const AMapPrivacyStatement amapPrivacyStatement =
+  AMapPrivacyStatement(hasContains: true, hasShow: true, hasAgree: true);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
+import 'dart:developer';
+
+import 'package:amap_map/amap_map.dart';
 import 'package:flutter/material.dart';
+import 'package:mapapp/const_config.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:x_amap_base/x_amap_base.dart';
+
+final List<Permission> needPermissionList = [
+  Permission.location,
+  Permission.storage,
+  Permission.phone,
+];
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +19,125 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  //
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    _checkPermissions();
+    AMapInitializer.init(context, apiKey: ConstConfig.amapApiKeys);
+    AMapInitializer.updatePrivacyAgree(ConstConfig.amapPrivacyStatement);
+    return const Surface();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void _checkPermissions() async {
+    Map<Permission, PermissionStatus> statuses =
+    await needPermissionList.request();
+    statuses.forEach((key, value) {
+      log('$key permissionStatus is $value');
     });
   }
+}
+
+class Surface extends StatefulWidget {
+  const Surface({super.key});
+
+  @override
+  _SurfaceWidgetState createState() => _SurfaceWidgetState();
+}
+
+class _SurfaceWidgetState extends State<Surface> {
+  var selectedPage = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    Widget page;
+    switch (selectedPage) {
+      case 0:
+        page = _ShowMapPageBody();
+        break;
+      case 1:
+        page = const SearchPage();
+        break;
+      default:
+        throw UnimplementedError('no page');
+    }
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("高德地图演示"),
+          ),
+          body: page,
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: selectedPage,
+            onTap: (int index) {
+              setState(() {
+                selectedPage = index;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.map),
+                label: 'Map',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Search',
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class _ShowMapPageBody extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ShowMapPageState();
+}
+
+class _ShowMapPageState extends State<_ShowMapPageBody> {
+  static const CameraPosition _kInitialPosition = CameraPosition(
+    target: LatLng(30.51279, 114.413487),
+    zoom: 17.0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final AMapWidget map = AMapWidget(
+      mapType: MapType.satellite,
+      compassEnabled: true,
+      labelsEnabled: false,
+      initialCameraPosition: _kInitialPosition,
+      onMapCreated: onMapCreated,
+    );
+
+    return ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: map,
+      ),
+    );
+  }
+
+  late AMapController _mapController;
+
+  void onMapCreated(AMapController controller) {
+    setState(() {
+      _mapController = controller;
+    });
   }
 }
